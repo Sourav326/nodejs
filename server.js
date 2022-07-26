3da7cca1e530a85d74519d2c3a78637a5d1eb6dd
sourav326 <souravchauhan1964@gmail.com>
2022-07-26 20:54:34 +0530

    Remove the node modules

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ http.createServer((req,res,next) => {
     res.end('Server Started');
     
 }).listen(3001,'localhost',()=>{
-    console.log('server running........');
+    console.log('server running........'); 
 });
