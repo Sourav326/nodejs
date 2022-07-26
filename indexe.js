1fb6d1e3931f4d646bf913f6b8a95f4f7c89e2c4
sourav326 <souravchauhan1964@gmail.com>
2022-07-26 23:34:15 +0530

    Add express package and create html page

--- indexe.js
+++ indexe.js
@@ -1,2 +1,22 @@
 const express = require('express');
 const app = express();
+
+const path = require('path');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+const port = 3001;
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname+"/index.html"));
+});
+
+app.post("/create-user",(req,res)=>{
+    res.send(`<h1>Firstname:- ${req.body.firstname}</h1> <h1>Lastname:- ${req.body.lastname}</h1> <h1>Country:- ${req.body.country}</h1>`)
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on port : ${port}`);
+});
